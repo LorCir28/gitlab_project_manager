4340d6ebec88c19621053173d3d65909861ac07c
blazk44 <blazkblazk@gmail.com>
2022-05-06 17:27:45 +0200

    listing projects

--- frontend/react-app/src/App.js
+++ frontend/react-app/src/App.js
@@ -3,36 +3,62 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.css"
 import Navbar from "./components/navbar"
 import Popup from "./components/popup"
-import {useState} from "react"
+import {useState, useEffect} from "react"
 import axios from "axios"
+import ProjectItem from "./components/projectItem";
 
 function App() {
   const [ButtonPopup, setButtonPopup] = useState(false);
+  // let Projects = axios.get("http://localhost:8000/projects");
+  // const getProjects = async() => {
+  //   let Projects = await axios.get("http://localhost:8000/projects");
+  //   return Projects;
+  // }
+  // const projects = () => {
+  //   axios.get("http://localhost:8000/projects")
+  //   .then(res => {return res.data})
+  // }
+
+  const [projects, setProjects] = useState([]);
+  useEffect(() => {
+    // async function fetchData() {
+    //   const dati = await axios.get("http://localhost:8000/projects");
+    //   setProjects(dati.data);
+    //   return dati;
+    // }
+    // fetchData();
+    fetch("http://localhost:8000/projects")
+    .then(res => {return res.json()})
+    .then(data => setProjects(data))
+  }, [])
+
+
+
   return (
-    <React.Fragment>
+    <>
       <Navbar></Navbar>
       <br />
       <br />
-      <button type="button" class="btn btn-secondary" onClick={() => setButtonPopup(true)}>Create new project</button>
+      <h1>Projects</h1>
+      <br />
+      {projects && projects.map((project) => (
+        <ProjectItem item={project} key={project.id}/>
+      ))}
+        {/* {getProjects()
+        .then(project => <h1>{project}</h1>)} */}
+      {/* <button type="button" class="btn btn-secondary" onClick={() => setButtonPopup(true)}>Create new project</button>
       <Popup trigger={ButtonPopup} setTrigger={setButtonPopup}>
-        <br />
-        {/* <form action="/project-creation" method="post"> */}
-          <input type={"text"} placeholder={"name of project"} id={"nameProject"}></input>
-          {/* <input type={"text"} placeholder={"name of label 1"} id={"nameLabel1"}></input>
-          <input type={"text"} placeholder={"color of label 1"} id={"colorLabel1"}></input> */}
-        {/* </form> */}
+        <input type={"text"} placeholder={"name of project"} id={"nameProject"}></input>
         <br />
         <br />
         <button type="button" class="btn btn-success" onClick={() => {
           const nameProject = document.getElementById("nameProject").value;
-          // const nameLabel1 = document.getElementById("nameLabel1").value;
-          // const colorLabel1 = document.getElementById("colorLabel1").value;
           axios.post(`http://localhost:8000/new_project/${nameProject}`);
         }}>
           create project
         </button>
-      </Popup>
-    </React.Fragment>
+      </Popup> */}
+    </>
   );
 }
 
---
+++ frontend/react-app/src/components/projectItem.js
@@ -0,0 +1,7 @@
+const ProjectItem = ({item}) => {
+    return(
+        <h2>{item.name}</h2>
+    )
+}
+
+export default ProjectItem;
